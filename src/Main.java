ddbd81d97a88eae810adce9e3a4e1e5bed3365de
Dimmi <sinrul86@gmail.com>
2021-02-28 23:14:00 +0300

    version 2.0 добавлена возможность установки значений для K

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,76 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Main {
 
     public static Map<String, String> resultMap(Path path) throws IOException {
-        Map<String,String> result = new HashMap<>();
-        List<String> lines = Files.readAllLines(path);
-        for (String line : lines){
-            if (line.startsWith("K1") || line.startsWith("K2") || line.startsWith("K3")){
-                result.put(line.split("=")[0], line.split("=")[1]);
+        Map<String, String> result = new HashMap<>();
+        BufferedReader reader = new BufferedReader(new FileReader(path.toString()));
+        while (reader.ready()) {
+            String line = reader.readLine();
+            if (line.startsWith("K")) {
+                String[] strings = line.split("=");
+                String key = strings[0];
+                String value = strings[1].replace(',', '.');
+                double val = Double.parseDouble(value);
+                switch (key) {
+                    case "K1":
+                        double minK1 = Double.parseDouble(View.list.get(0).getText());
+                        double maxK1 = Double.parseDouble(View.list.get(1).getText());
+                        if (val < minK1 || val > maxK1) {
+                            result.put(key, value);
+                        }
+                        break;
+                    case "K2":
+                        double minK2 = Double.parseDouble(View.list.get(2).getText());
+                        double maxK2 = Double.parseDouble(View.list.get(3).getText());
+                        if (val < minK2 || val > maxK2) {
+                            result.put(key, value);
+                        }
+                        break;
+                    case "K3":
+                        double minK3 = Double.parseDouble(View.list.get(4).getText());
+                        double maxK3 = Double.parseDouble(View.list.get(5).getText());
+                        if (val < minK3 || val > maxK3) {
+                            result.put(key, value);
+                        }
+                        break;
+                    case "K4":
+                        double minK4 = Double.parseDouble(View.list.get(6).getText());
+                        double maxK4 = Double.parseDouble(View.list.get(7).getText());
+                        if (val < minK4 || val > maxK4) {
+                            result.put(key, value);
+                        }
+                        break;
+                    case "K5":
+                        double minK5 = Double.parseDouble(View.list.get(8).getText());
+                        double maxK5 = Double.parseDouble(View.list.get(9).getText());
+                        if (val < minK5 || val > maxK5) {
+                            result.put(key, value);
+                        }
+                        break;
+                    case "K6":
+                        double minK6 = Double.parseDouble(View.list.get(10).getText());
+                        double maxK6 = Double.parseDouble(View.list.get(11).getText());
+                        if (val < minK6 || val > maxK6) {
+                            result.put(key, value);
+                        }
+                        break;
+                    case "K7":
+                        double minK7 = Double.parseDouble(View.list.get(12).getText());
+                        double maxK7 = Double.parseDouble(View.list.get(13).getText());
+                        if (val < minK7 || val > maxK7) {
+                            result.put(key, value);
+                        }
+                        break;
+                }
             }
         }
+        reader.close();
         return result;
     }
 
@@ -25,18 +78,20 @@ public class Main {
         BufferedWriter writer = new BufferedWriter(new FileWriter(result, true));
         try (DirectoryStream<Path> files = Files.newDirectoryStream(path)) {
             for (Path path1 : files) {
-                if (Files.isRegularFile(path1)){
+                if (Files.isRegularFile(path1)) {
                     Map<String, String> map = resultMap(path1);
-                    View.textArea.append(path1.getFileName().toAbsolutePath() + ":\n");
-                    writer.write(path1.toAbsolutePath() + ":\n");
-                    for (String s : map.keySet()) {
-                        View.textArea.append(s + " = " + map.get(s) + "\n");
-                        writer.write(s + " = " + map.get(s) + "\n");
+                    if (map.size() > 0) {
+                        View.textArea.append(path1.getFileName() + ":\n");
+                        writer.write(path1.getFileName().toString().substring(0,path1.getFileName().toString().length() -4));
+                        for (String s : map.keySet()) {
+                            View.textArea.append(s + " = " + map.get(s) + "\n");
+//                            writer.write(s + " = " + map.get(s) + "\n");
+                        }
+                        View.textArea.append("\n");
+                        writer.write("\n");
                     }
-                    View.textArea.append("\n");
-                    writer.write("\n");
                 }
-                if (Files.isDirectory(path1)){
+                if (Files.isDirectory(path1)) {
                     isDirectory(path1, result);
                 }
             }
--- src/View.java
+++ src/View.java
@@ -5,31 +5,72 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class View {
     public static JFrame frame = new JFrame("Search");
     private static JButton open = new JButton("Open");
-    private static JTextField source = new JTextField(20);
-    private static JTextField dest = new JTextField(20);
-    public static JTextArea textArea = new JTextArea(30,30);
+    private static JButton save = new JButton("Save");
+    private static JTextField source = new JTextField(10);
+    private static JTextField dest = new JTextField(10);
+    public static JTextArea textArea = new JTextArea(20, 30);
+    public static List<JTextField> list = new ArrayList<JTextField>();
 
     public static void initView() throws IOException {
-        frame.setSize(850,600);
+        frame.setSize(400, 600);
         JButton search = new JButton("Поиск");
+        JButton reset = new JButton("Сброс");
 
-        JPanel panel = new JPanel();
+        JPanel panel = new JPanel(new GridLayout(2, 3, 10, 10));
         panel.add(new JLabel("Где искать"));
         panel.add(source);
         panel.add(open);
-        panel.add(new JLabel("Куда сохранить результат"));
+        panel.add(new JLabel("Куда сохранить"));
         panel.add(dest);
+        panel.add(save);
+
         frame.getContentPane().add(BorderLayout.NORTH, panel);
 
-        JPanel panel2 = new JPanel();
-        panel2.add(textArea);
+        JPanel panelK = new JPanel(new GridLayout(9, 3, 10, 10));
+        JTextField fieldMin;
+        JTextField fieldMax;
+        panelK.add(new JLabel());
+        panelK.add(new JLabel());
+        panelK.add(new JLabel());
+        panelK.add(new JLabel("Коэффициенты"));
+        panelK.add(new JLabel("Минимум"));
+        panelK.add(new JLabel("Максимум"));
+        for (int i = 1; i < 8; i++) {
+            fieldMin = new JTextField(10);
+            fieldMax = new JTextField(10);
+            panelK.add(new JLabel("K" + i));
+            if (i == 1) {
+                fieldMin.setText("0.93");
+                fieldMax.setText("1.07");
+            } else if (i == 2){
+                fieldMin.setText("0.94");
+                fieldMax.setText("1.06");
+            } else {
+                fieldMin.setText("0.945");
+                fieldMax.setText("1.055");
+            }
+            panelK.add(fieldMin);
+            panelK.add(fieldMax);
+            list.add(fieldMin);
+            list.add(fieldMax);
+        }
+        JPanel flow = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        flow.add(panelK);
+        frame.getContentPane().add(flow, BorderLayout.CENTER);
 
-        frame.getContentPane().add(new JScrollPane(textArea),BorderLayout.CENTER);
-        frame.getContentPane().add(BorderLayout.AFTER_LAST_LINE, search);
+        frame.add(new JScrollPane(textArea), BorderLayout.EAST);
+
+        JPanel panel2 = new JPanel();
+        panel2.add(search);
+        panel2.add(reset);
+        frame.getContentPane().add(BorderLayout.SOUTH, panel2);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.pack();
@@ -37,7 +78,7 @@ public class View {
         search.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Path path = Path.of(open.getText());
+                Path path = Path.of(source.getText());
                 try {
                     Main.isDirectory(path, dest.getText());
                 } catch (IOException ioException) {
@@ -50,16 +91,47 @@ public class View {
             @Override
             public void actionPerformed(ActionEvent e) {
                 File file = null;
-                JFileChooser fileopen = new JFileChooser();
-                fileopen.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-                int ret = fileopen.showDialog(null, "Открыть...");
+                JFileChooser fileOpen = new JFileChooser();
+                fileOpen.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                fileOpen.setCurrentDirectory(new File("D:\\"));
+                int ret = fileOpen.showOpenDialog(null);
                 if (ret == JFileChooser.APPROVE_OPTION) {
-                    file = fileopen.getSelectedFile();
+                    file = fileOpen.getSelectedFile();
                     source.setText(file.getAbsolutePath());
                 }
             }
         });
+
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                File file = null;
+                JFileChooser fileSave = new JFileChooser();
+                fileSave.setCurrentDirectory(new File("D:\\"));
+                int ret = fileSave.showSaveDialog(null);
+                if (ret == JFileChooser.APPROVE_OPTION) {
+                    file = fileSave.getSelectedFile();
+                    dest.setText(file.getAbsolutePath());
+                }
+            }
+        });
+        reset.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                reset();
+            }
+        });
     }
+
+    private static void reset() {
+        textArea.setText("");
+        source.setText("");
+        dest.setText("");
+        for (JTextField field : list) {
+            field.setText("");
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         View.initView();
     }
